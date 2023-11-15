d23d30db255eab1f0f344135fc0df9e4b43906a5
David Santiago Martínez <davids.martinezl@uqvirtual.edu.co>
2023-11-15 15:04:15 -0500

    Front End de P. Avanzada V 1.1.0 - Se vuelve a crear el proyecto pero con una estructura bien. Se necesitan crear todavia los modulos que faltan del proyect (clases), cambiar formularios para que tengan opción de select y seguir con el resto de las guías 16 en adelante

---
+++ src/app/pagina/registro/registro.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent {
+
+  //para enlazar input del formulario
+  registroPacienteDTO: RegistroPacienteDTO;
+  constructor(){
+  this.registroPacienteDTO = new RegistroPacienteDTO();
+  }
+
+  //integración del back por API REST
+  public registrar(){
+    console.log(this.registroPacienteDTO);
+    }
+
+  //Confirmar password
+  public sonIguales():boolean{
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+    }
+
+}
+  
+
+
