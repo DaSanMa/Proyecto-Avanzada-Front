1586a03c24d7cee10b7f2a5db167bd5df2839c5a
David Santiago Martínez <davids.martinezl@uqvirtual.edu.co>
2023-11-16 09:46:24 -0500

    Se crea el login falta el resto de formularios

--- src/app/pagina/login/login.component.ts
+++ src/app/pagina/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { LoginPacienteDTO } from 'src/app/modelo/login-paciente-dto';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent {
+
+  loginPacienteDTO: LoginPacienteDTO;
+
+  //constructor
+  constructor (){
+  this.loginPacienteDTO = new LoginPacienteDTO();
+
+  }
+  //integración del back por API REST
+  public ingresar(){
+    
+    console.log(this.loginPacienteDTO);
+
+  }
 
 }
