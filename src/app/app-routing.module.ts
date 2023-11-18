015ac9571303045a0d79369d74deffe8d98ee801
David Santiago Martínez <davids.martinezl@uqvirtual.edu.co>
2023-11-18 00:33:00 -0500

    Se hacen la Gestión de PQRS, creación de los mismo
    Quedan pendientes por hacer el resto de formularios.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
+import { GestionPqrsComponent } from './pagina/gestion-pqrs/gestion-pqrs.component';
+import { CrearPqrsComponent } from './pagina/crear-pqrs/crear-pqrs.component';
+import { DetallePqrsComponent } from './pagina/detalle-pqrs/detalle-pqrs.component';
+
+
 const routes: Routes = [
-{ path: "", component: InicioComponent },
-{ path: "login", component: LoginComponent },
-{ path: "registro", component: RegistroComponent },
-{ path: "**", pathMatch: "full", redirectTo: "" }
+    { path: "", component: InicioComponent },
+    { path: "login", component: LoginComponent },
+    { path: "registro", component: RegistroComponent },
+    { path: "gestion-pqrs", component: GestionPqrsComponent },
+    { path: "crear-pqrs", component: CrearPqrsComponent },
+    { path: "detalle-pqrs/:codigo", component: DetallePqrsComponent },
+    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
+
 @NgModule({
-imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
